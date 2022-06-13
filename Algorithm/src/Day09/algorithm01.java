38696cf9f1c0454c41f9bed0340fabb90e1f8f87
jaehee <jaehee3679@daum.net>
2022-06-14 08:12:29 +0900

    hhh

---
+++ Algorithm/src/Day09/algorithm01.java
@@ -0,0 +1,30 @@
+package Day09;
+
+import java.util.Scanner;
+
+public class algorithm01 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("점수 입력하쇼");
+		int score=sc.nextInt();
+		if(score>=90 && score<=100) {
+			System.out.println("A");
+		}else if(score>=80 && score <=89) {
+			System.out.println("B");
+		}else if(score>=70 && score<=79) {
+			System.out.println("C");
+		}else if(score>=60 && score<=69) {
+			System.out.println("D");
+		}else {
+			System.out.println("F");
+		}
+		
+	}
+
+	
+		
+}
+
+
---
+++ Algorithm/src/Day09/algorithm08.java
@@ -0,0 +1,20 @@
+package Day09;
+
+import java.util.Scanner;
+
+public class algorithm08 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int count=sc.nextInt();
+		int n=0;
+	
+		while(n==count) {
+			
+		
+			n++;
+		}
+
+	}
+}
