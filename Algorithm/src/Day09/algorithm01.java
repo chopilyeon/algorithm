4e56d882a4230eebad65a69ffdceea3d6f2522b9
조재희 <jaehee3679@daum.net>
2022-06-15 10:12:32 +0900

    hhhh

--- Algorithm/src/Day09/algorithm01.java
+++ Algorithm/src/Day09/algorithm01.java
@@ -8,7 +8,7 @@ public class algorithm01 {
 
 		Scanner sc = new Scanner(System.in);
 		
-		int score=sc.nextInt();
+		int score=int ;
 		if(score>=90 && score<=100) {
 			System.out.println("A");
 		}else if(score>=80 && score <=89) {
---
+++ Algorithm/src/Day10/algorithm03.java
@@ -0,0 +1,10 @@
+package Day10;
+
+public class algorithm03 {
+
+	public static void main(String[] args) {
+	
+		System.out.println("hhhh");
+	}
+
+}
